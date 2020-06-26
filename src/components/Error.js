d9ad9de2db0993bc7cb0a458b56a9f54f28fa1b6
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-06-26 07:22:58 +0200

    creating Error component

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,9 +2,9 @@ import React from "react";
 const Error = ({ message }) => {
 	return (
 		<div className="container">
-			<div className="alert alert-error">
-				<h5 className="text text-center">{message}</h5>
-			</div>
+			<div className="alert alert-danger">
+				<h5 className="text text-center"> {message} </h5>{" "}
+			</div>{" "}
 		</div>
 	);
 };
