bfc72609fb4d9b96ee6f8ab91760f98417dfb292
J.C. Beneyto <jnk.beneyto@gmail.com>
2020-06-26 07:23:57 +0200

    setBudget and Form working

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { Fragment, useState } from "react";
 import SetBudget from "./components/SetBudget";
-import Error from "./components/Error";
+// import Error from "./components/Error";
 import Form from "./components/Form";
 
 import "./App.css";
 
 function App() {
-	// set default Error state false
-	const [error, setError] = useState(false);
+	//setting total budget to 0
+	const [totalBudget, setTotalBudget] = useState(0);
+
+	//setting diff budget to 0
+	const [diffBudget, setDiffBudget] = useState(0);
+
+	//showing components
+	const [showSetBudget, setShowSetBudget] = useState(true);
+
+	//setting outcomes
+	const [outcomes, setOutcomes] = useState([]);
+
+	//function sent to Form in order to use there
+	const addNewOutcomes = outcome => {
+		setOutcomes([...outcomes, outcome]);
+		console.log(outcomes);
+	};
 
 	return (
 		<Fragment>
 			<div className="container mt-3">
 				<h1 className="text text-center">Budget Controller</h1>
-				<div className="row">{error ? <Error /> : null}</div>
-				<div className="row">
-					<SetBudget />
-				</div>
-				<div className="row">
-					<div className="col ">
-						<Form />
+				{showSetBudget ? (
+					<div className="row">
+						<SetBudget
+							setTotalBudget={setTotalBudget}
+							setDiffBudget={setDiffBudget}
+							setShowSetBudget={setShowSetBudget}
+						/>
 					</div>
-					<div className="col bg-primary">2</div>
-				</div>
+				) : (
+					<div className="row">
+						<div className="col ">
+							<Form addNewOutcomes={addNewOutcomes} />
+						</div>
+						<div className="col bg-primary">
+							<div className="container">list of outcomes</div>
+						</div>
+					</div>
+				)}
 			</div>
 		</Fragment>
 	);
